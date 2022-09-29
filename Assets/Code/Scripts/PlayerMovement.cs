7a6367bcff39f6ffd2b496a35dcde19aa68efd57
Derek <derekhmcinerney@gmail.com>
2022-09-29 12:37:51 -0400

    added player sprite and movement

---
+++ Assets/Code/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    public Rigidbody2D rb;
+    private bool facingRight = true;
+
+    private float moveDirection;
+
+    private bool isJumping = false;
+    // Awake is called after objects are initialized. Called in a random order
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+
+        Animate();
+    }
+
+    // better for handling physics
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Animate()
+    {
+        // Animate
+        if (moveDirection > 0 && !facingRight)
+        {
+            FlipCharacter();
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            FlipCharacter();
+        }
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+        if (isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+        }
+
+        isJumping = false;
+    }
+    private void ProcessInputs()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+    }
+    private void FlipCharacter()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
