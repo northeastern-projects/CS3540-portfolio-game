a136e61a403980e779baff03bc4c6b21b638ef5b
Derek <derekhmcinerney@gmail.com>
2022-10-12 23:18:17 -0400

    updated animation handler & sprinting, Dash broke

--- Assets/Code/Scripts/PlayerMovement.cs
+++ Assets/Code/Scripts/PlayerMovement.cs
@@ -2,19 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
-
     public float moveSpeed;
+    public float sprintSpeed;
     public float jumpForce;
-    
+
+    [SerializeField] [Tooltip("Insert Animator Controller")]
+    private Animator playerAnimator;
+
+
     // Dashing
     public float dashForce;
-    private float _dashTime = 0.1f;
+    private const float DashTime = 0.1f;
     private bool _canDash = true;
     private bool _isDashing;
-    public float _dashingCooldown;
+    public float dashingCooldown;
     [SerializeField] private TrailRenderer tr;
+
 
     public Rigidbody2D rb;
     private bool _facingRight = true;
@@ -23,8 +29,13 @@ public class PlayerMovement : MonoBehaviour
     private float _moveVertical;
     private bool _startDash;
     private bool _isJumping;
+    private bool _isRunning;
+    private static readonly int IsJumping = Animator.StringToHash("isJumping");
+    private static readonly int IsDashing = Animator.StringToHash("isDashing");
+    private static readonly int IsWalking = Animator.StringToHash("isWalking");
+    private static readonly int IsRunning = Animator.StringToHash("isRunning");
 
-    
+
     // Awake is called after objects are initialized. Called in a random order
     private void Awake()
     {
@@ -38,15 +49,20 @@ public class PlayerMovement : MonoBehaviour
         {
             return;
         }
-        
+
         ProcessInputs();
 
         Animate();
-        
+
         if (_startDash && _canDash)
         {
             StartCoroutine(Dash());
         }
+
+        // Walking and Sprinting
+        rb.velocity = _isRunning
+            ? new Vector2(_moveDirection * sprintSpeed, rb.velocity.y)
+            : new Vector2(_moveDirection * moveSpeed, rb.velocity.y);
     }
 
     // better for handling physics
@@ -56,7 +72,7 @@ public class PlayerMovement : MonoBehaviour
         {
             return;
         }
-        
+
         Move();
     }
 
@@ -71,32 +87,36 @@ public class PlayerMovement : MonoBehaviour
         {
             FlipCharacter();
         }
+
+        playerAnimator.SetBool(IsJumping, _isJumping);
+        playerAnimator.SetBool(IsDashing, _isDashing);
+        playerAnimator.SetBool(IsRunning, _isRunning);
+        playerAnimator.SetBool(IsWalking, rb.velocity.magnitude > 0);
     }
 
     private void Move()
     {
-        // Walking
-        rb.velocity = new Vector2(_moveDirection * moveSpeed, rb.velocity.y);
-        
         // Jumping
         if (!_isJumping && _moveVertical > 0.1f)
         {
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         }
     }
+
     private void ProcessInputs()
     {
         _moveDirection = Input.GetAxis("Horizontal");
         _moveVertical = Input.GetAxis("Vertical");
         _startDash = Input.GetKeyDown(KeyCode.LeftControl);
-        
+        _isRunning = Input.GetKey(KeyCode.LeftShift);
     }
+
     private void FlipCharacter()
     {
         _facingRight = !_facingRight;
         transform.Rotate(0f, 180f, 0f);
     }
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Platform"))
@@ -121,11 +141,11 @@ public class PlayerMovement : MonoBehaviour
         rb.gravityScale = 0f;
         rb.velocity = _facingRight ? new Vector2(dashForce, 0f) : new Vector2(-dashForce, 0f);
         tr.emitting = true;
-        yield return new WaitForSeconds(_dashTime);
+        yield return new WaitForSeconds(DashTime);
         tr.emitting = false;
         rb.gravityScale = originalGravity;
         _isDashing = false;
-        yield return new WaitForSeconds(_dashingCooldown);
+        yield return new WaitForSeconds(dashingCooldown);
         _canDash = true;
     }
 }
