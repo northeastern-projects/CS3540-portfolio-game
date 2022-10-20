2b9d912c6a8877790f69ba1b1fb206955524c888
Eddie Zhou <ace.dragonfly@gmail.com>
2022-10-19 23:28:24 -0400

    Add coin spawning randomness and more spawnpoints

--- Assets/Code/Scripts/ItemSpawner.cs
+++ Assets/Code/Scripts/ItemSpawner.cs
@@ -6,12 +6,16 @@ using Random = UnityEngine.Random;
 
 public class ItemSpawner : MonoBehaviour
 {
+    [SerializeField] private float spawnChance = 1.0f;
     [SerializeField] private GameObject[] items;
     
     // Start is called before the first frame update
     private void Start()
     {
-        Instantiate(RandomItem(), transform);
+        if (Random.Range(0.0f, 1.0f) < spawnChance)
+        {
+            Instantiate(RandomItem(), transform);
+        }
     }
 
     private GameObject RandomItem()
